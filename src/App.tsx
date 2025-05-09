4868f2e00478ba3446f63ee9f1f4d39b7ee3f16d
Julio-Vasquez <jualvalitube@gmail.com>
2025-05-09 15:48:37 -0500

    feat: add Redux provider and render routes conditionally based on auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useSelector } from 'react-redux'
+import { RouterProvider } from 'react-router'
+
+import DashboardLayout from './layout/DashboardLayout'
+
+import type { RootState } from './store/Reducers'
+import { privateRouter, publicRouter } from './routes'
+import { AUTH } from './utils/constants/redux.constant'
+
 const App = () => {
-  return <div>Hey</div>
+  const { authentication: auth } = useSelector(
+    (state: RootState) => state[AUTH]
+  )
+
+  return !auth ? (
+    <RouterProvider router={publicRouter} />
+  ) : (
+    <DashboardLayout>
+      <RouterProvider router={privateRouter} />
+    </DashboardLayout>
+  )
 }
 
 export default App
